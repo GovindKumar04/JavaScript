1f68e6ece97da44cb3d951f0a11f6e63acd0fe3e
Govind kumar <134090540+GovindKumar04@users.noreply.github.com>
2025-10-12 12:43:23 +0530

    ConversionOperation

---
+++ conversionoperation.js
@@ -0,0 +1,11 @@
+let a = "hello";
+let b= "Govind";
+let c=7;
+console.log(a-c);
+console.log(c-a);
+console.log(typeof a-b);
+console.log(typeof (a-b));
+let d=a-b;
+console.log(d);
+console.log(3+4*5/3);
+
