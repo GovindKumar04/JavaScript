f4ca4a435c0ed22fd16fda77701449d29b1e7e02
Govind kumar <134090540+GovindKumar04@users.noreply.github.com>
2025-10-12 12:24:02 +0530

    Conversion

---
+++ conversionOfDatatTypes.js
@@ -0,0 +1,16 @@
+let a=4;
+let b="8";
+
+console.log(typeof(a));
+console.log(typeof(b));
+let c=parseInt(b);
+let d= Number(b)
+console.log(typeof c);
+console.log(typeof d);
+
+let e= -8;
+let f=Boolean(e)
+// Any number except 0 -0 and NaN are true ans for string only empty string are false
+console.log(f);
+
+
